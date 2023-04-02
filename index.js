4dfa81b4a3e2d34d6651a1a8319275cd9808dc0f
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2023-04-02 16:00:32 +0300

    commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+
+
+
+    const ingredients = [
+    {
+        "id": "1",
+        "item": "Bacon"
+    },
+    {
+        "id": "2",
+        "item": "Eggs"
+    },
+    {
+        "id": "3",
+        "item": "Milk"
+    },
+    {
+        "id": "4",
+        "item": "Butter"
+    }
+];
+
+app.use(cors({
+    origin: "http://89.117.36.197"
+}))
+
+app.get('/ingredients', (req, res) =>{
+    res.send(ingredients); 
+});
+app.listen(3000);
